7fc1c3d9b62e86accc8455641f7982375460841c
Platon Solovianov <platonsolovyanov@gmail.com>
2021-01-20 23:24:16 +0300

    add db / refactoring container,pages / add  prop-types,axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Header} from "./container/index";
-import {Home, Cart}  from "./pages";
-import {Route} from 'react-router-dom';
+import { Header } from "./container/index";
+import { Home, Cart } from "./pages";
+import { Route } from 'react-router-dom';
+import axios from "axios";
 
 function App() {
+	const [pizzas, setPizzas] = React.useState([]);
+
+	React.useEffect(() => {
+		axios.get('http://localhost:3000/db.json')
+			.then(({ data }) => {
+				setPizzas(data.pizzas)
+			})
+	}, [])
+
 	return (
 		<div className='wrapper'>
 			<Header />
-			<Route path="/" component={Home} exact />
+			<Route path="/" render={() => <Home items={pizzas} />} exact />
 			<Route path="/cart" component={Cart} exact />
-			
+
 		</div>
 	);
 }
--- src/container/Button.jsx
+++ src/container/Button.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import cn from "classnames";
+import pt from 'prop-types';
 
-function Button({onClick, className, outline, headerBtn, children}){ 
-              
+function Button({ onClick, className, outline, headerBtn, children }) {
+
         return (
-        <button
-        onClick={onClick}
-        className={cn('btn',
-        {'btn-outline' : outline}, {'cart__button-header' : headerBtn})}>
-        {children}
-        </button>)
-    
+                <button
+                        onClick={onClick}
+                        className={cn('btn',
+                                { 'btn-outline': outline }, { 'cart__button-header': headerBtn })}>
+                        {children}
+                </button>)
+
+}
+
+Button.propTypes = {
+        onClick: pt.func,
 }
 
 export default Button;
